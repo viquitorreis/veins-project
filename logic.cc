45601797d5588c00b47e45f5d0b12638086c744a
victor <victorreisprog@gmail.com>
2024-11-04 06:50:05 -0300

    feat: more nodes

--- logic.cc
+++ logic.cc
@@ -32,8 +32,19 @@ void OBU::initialize()
         // vai criar e enviar a primeira mensagem no gate "out"
         // uma string qualquer pode ser definida como 'nome' do tipo da mensagem dentro do objeto
         // instanciado cMessage
-        cMessage *msg = new cMessage("InfoRequest");
-        send(msg, "out");
+	    // std::string messageName = "InfoRequest_" + std::to_string(getIndex());
+        // cMessage *msg = new cMessage(messageName.c_str());
+	    // // precisamos checar se o gate do OBU já está conectado, se não vai bugar
+	    // // https://doc.omnetpp.org/omnetpp4/api/classcGate.html#a458a23f084b6e561598cee4876dd3669
+        // if (!gate("out")->isConnectedOutside()) {
+		//     send(msg, "out");
+        // } else {
+        //     EV << "Gate 'out' já  está conectado.\n";
+        // }
+        if (strcmp("obu", getName()) == 0) {
+            cMessage *msg = new cMessage("InfoRequest");
+            send(msg, "out");
+        }
     }
 }
 
@@ -45,6 +56,9 @@ void OBU::handleMessage(cMessage *msg)
     // send(msg, "out");
     EV << "Mensagem recebida pelo OBU: " << msg->getName() << "\n";
     delete msg; // limpando a mensagem depois de processar ela
+
+    cMessage *response = new cMessage("TouristInfoResponse");
+    send(response, "out");
 }
 
 // RSU vai processar a requisição (mensagem do OBU) e então vai enviar a resposta
@@ -54,6 +68,24 @@ void RSU::handleMessage(cMessage *msg)
     delete msg; // limpando a mensagem recebida
 
     // cria e envia a resposta de volta para o OBU
-    cMessage *response = new cMessage("TouristInfoRespose");
-    send(response, "out");
+    // cMessage *response = new cMessage("TouristInfoRespose");
+    // send(response, "out", 0);
+
+    // iterando pelos OBUs
+    // for (int i = 0; i < getParentModule()->par("numOBUs").intValue(); i++) {
+    //     std::string obuPath = "CityNetwork.obu[" + std::to_string(i) + "]";
+    //     cModule *obuModule = getSimulation()->getModuleByPath(obuPath.c_str());
+
+    //     if (obuModule) {
+    //         cMessage *response = new cMessage("TouristInfoResponse");
+    //         send(response, "out", i); // enviando no gateway array out[i]
+    //     }
+    // }
+
+    // getParentModule() -> https://doc.omnetpp.org/omnetpp/api/classomnetpp_1_1cModule.html#a6273eb514331e0e44130844d637b8b4c
+    EV << "[numOBUs]: " << getParentModule()->par("numOBUs").intValue() << "\n";
+    for (int i = 0; i < getParentModule()->par("numOBUs").intValue(); i++) {
+        cMessage *response = new cMessage("TouristInfoResponse");
+        send(response, "out", i); // enviando via gate array out[i]
+    }
 }
