7e9a47cc448d0a98abb32727914b21e7a8262a5b
victor <victorreisprog@gmail.com>
2024-11-10 20:33:55 -0300

    feat: random advertsements, more OBUs

--- logic.cc
+++ logic.cc
@@ -1,91 +1,81 @@
 #include <string.h>
 #include <omnetpp.h>
+#include <vector>
+#include <unordered_set>
+#include <cstdlib>   // para randomização
 
 using namespace omnetpp;
 
-// classe RSU vai definir / assinar os métodos referentes ao RSU
-class RSU: public cSimpleModule
-{
-    protected:
-    // Função virtual redefinida que vai conter os algoritmos implementados.
-        virtual void handleMessage(cMessage *msg) override;
+class RSU : public cSimpleModule {
+protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+
+private:
+    std::vector<std::string> advertisements; // lista de propagandas
+    std::unordered_set<int> assignedOBUs; // set para registrar os OBUs que receberam as propagandas
 };
 
-class OBU: public cSimpleModule
-{
-    protected:
-        // função virtual que vai inicializar tudo. É chamada só na inicialização do programa
-        virtual void initialize() override;
-        virtual void handleMessage(cMessage *msg) override;
+class OBU : public cSimpleModule {
+protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
 };
 
-// Precisamos registrar os módulos com o omnetpp
+// Registrando os módulos com o omnetpp
 Define_Module(RSU);
 Define_Module(OBU);
 
-void OBU::initialize()
-{
-    // o método initialize é chamado no começo da simulação para fazer o bootstrap da simulação
-    // um dos módulos precisa enviar a mensagem primerio
+void RSU::initialize() {
+    // inicializando a lista de propagandas
+    advertisements.push_back("Complexo Parque do Sabiá");
+    advertisements.push_back("Museu Municipal de Uberlândia");
+    advertisements.push_back("Mercado Municipal");
+    advertisements.push_back("Center Convention Uberlândia");
+    advertisements.push_back("Center Shopping");
 
-    if (strcmp("obu", getName()) == 0) {
-        // vai criar e enviar a primeira mensagem no gate "out"
-        // uma string qualquer pode ser definida como 'nome' do tipo da mensagem dentro do objeto
-        // instanciado cMessage
-	    // std::string messageName = "InfoRequest_" + std::to_string(getIndex());
-        // cMessage *msg = new cMessage(messageName.c_str());
-	    // // precisamos checar se o gate do OBU já está conectado, se não vai bugar
-	    // // https://doc.omnetpp.org/omnetpp4/api/classcGate.html#a458a23f084b6e561598cee4876dd3669
-        // if (!gate("out")->isConnectedOutside()) {
-		//     send(msg, "out");
-        // } else {
-        //     EV << "Gate 'out' já  está conectado.\n";
-        // }
-        if (strcmp("obu", getName()) == 0) {
-            cMessage *msg = new cMessage("InfoRequest");
-            send(msg, "out");
+    EV << "RSU initialized with " << advertisements.size() << " advertisements.\n";
+}
+
+void RSU::handleMessage(cMessage *msg) {
+    EV << "RSU recebeu a requisição: " << msg->getName() << "\n";
+    delete msg; // removendo a mensagem recebida
+
+    int numOBUs = getParentModule()->par("numOBUs").intValue();
+
+    for (int i = 0; i < numOBUs; i++) {
+        // checando se esse OBU já recebeu uma propaganda
+        // método .end() vai retornar o iterador para o final do set
+        if (assignedOBUs.find(i) != assignedOBUs.end()) {
+            EV << "OBU[" << i << "] já recebeu um anúncio. Ignorando...\n";
+            continue;
         }
+
+        // Randomicamente selecionando um anúncio para esse OBU
+        int randomIndex = rand() % advertisements.size();
+        std::string selectedAdvertisement = advertisements[randomIndex];
+
+        // cria e envia a mensagem com a propaganda selecionada
+        cMessage *response = new cMessage(selectedAdvertisement.c_str());
+        send(response, "out", i);
+
+        // marca o OBU como tendo recebido uma propaganda
+        assignedOBUs.insert(i);
+        EV << "Enviando anúncio para OBU[" << i << "]: " << selectedAdvertisement << "\n";
+
     }
 }
 
-void OBU::handleMessage(cMessage *msg)
-{
-    // o método handleMessage() é chamado sempre que um mensagem chega naquele módulo específico
-    // nós só enviamos para outro módulo, através do gate 'out'.
-    // no caso
-    // send(msg, "out");
+void OBU::initialize() {
+    // só envia a mensagem de requisição se o módulo for um OBU
+    if (strcmp("obu", getName()) == 0) {
+        cMessage *msg = new cMessage("InfoRequest");
+        send(msg, "out");
+    }
+}
+
+void OBU::handleMessage(cMessage *msg) {
     EV << "Mensagem recebida pelo OBU: " << msg->getName() << "\n";
-    delete msg; // limpando a mensagem depois de processar ela
-
-    cMessage *response = new cMessage("TouristInfoResponse");
-    send(response, "out");
-}
-
-// RSU vai processar a requisição (mensagem do OBU) e então vai enviar a resposta
-void RSU::handleMessage(cMessage *msg)
-{
-    EV << "RSU recebeu a requisição: " << msg-> getName() << "\n";
-    delete msg; // limpando a mensagem recebida
-
-    // cria e envia a resposta de volta para o OBU
-    // cMessage *response = new cMessage("TouristInfoRespose");
-    // send(response, "out", 0);
-
-    // iterando pelos OBUs
-    // for (int i = 0; i < getParentModule()->par("numOBUs").intValue(); i++) {
-    //     std::string obuPath = "CityNetwork.obu[" + std::to_string(i) + "]";
-    //     cModule *obuModule = getSimulation()->getModuleByPath(obuPath.c_str());
-
-    //     if (obuModule) {
-    //         cMessage *response = new cMessage("TouristInfoResponse");
-    //         send(response, "out", i); // enviando no gateway array out[i]
-    //     }
-    // }
-
-    // getParentModule() -> https://doc.omnetpp.org/omnetpp/api/classomnetpp_1_1cModule.html#a6273eb514331e0e44130844d637b8b4c
-    EV << "[numOBUs]: " << getParentModule()->par("numOBUs").intValue() << "\n";
-    for (int i = 0; i < getParentModule()->par("numOBUs").intValue(); i++) {
-        cMessage *response = new cMessage("TouristInfoResponse");
-        send(response, "out", i); // enviando via gate array out[i]
-    }
+    // limpando a msg depois de processar
+    delete msg;
 }
